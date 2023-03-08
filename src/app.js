32549a81fa629784a1f55710be24522e1365554e
nickolasvm <nickolas64vm@gmail.com>
2023-03-08 16:05:53 -0300

    feat: Hello World na API

--- src/app.js
+++ src/app.js
@@ -3,4 +3,7 @@ const express = require('express');
 
 const app = express();
 
+// rota http://localhost:3001/
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
 module.exports = app;
