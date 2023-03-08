8c8b3041b9b65200d743d28028b293e7940bfcd9
nickolasvm <nickolas64vm@gmail.com>
2023-03-08 16:37:34 -0300

    feat: Listando times por meio do método GET

--- src/app.js
+++ src/app.js
@@ -4,6 +4,25 @@ const express = require('express');
 const app = express();
 
 // rota http://localhost:3001/
-app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+// const OK = 200;
+// const INTERNAL_SERVER_ERROR = 500;
+// const NOT_FOUND = 404;
+
+// app.get('/', (req, res) => res.status(OK).json({ message: 'Olá Mundo!' }));
+
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
 module.exports = app;
